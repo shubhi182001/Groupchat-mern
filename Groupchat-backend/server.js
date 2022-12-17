3aca1beceff14b627f770f6e1673582cabf37c37
shubhi182001 <kediashubhi@gmail.com>
2022-12-17 10:18:11 +0530

    resolved CORS and added pusher

--- Groupchat-backend/server.js
+++ Groupchat-backend/server.js
@@ -1,11 +1,55 @@
 //type = "modules" in package.json adding it enables the use of ES6 modules now we can use import in place of require
-// import express from 'express';
+
+//Why are we using pusher?
+//We use firebase to make real time applications(use of socket io). But we can't do that with mongodb, we have to refresh it again and again after any change is made(or after a message is created or deleted we need to refresh then only it will get reflected), but pusher is a service which makes mongodb real time .
+//What we will do is , we will introduce changeStream in mongodb , so whenever there is a change in any collection at that time the change stream is going to trigger the pusher that is upload that message on pusher and we connect the pusher to our frontend.Pusher server will trigger down the data and push it to frontend.--> makes it real time.
+
+
+
 const e = require("express");
 const express = require("express")
 require('dotenv').config()
 const mongoose = require("mongoose");
 const Messages = require("./models/dbMessages")
 const app = express();
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: "1526204",
+    key: "ed94a8b9ce8932a13626",
+    secret: "4e710500647870d6359e",
+    cluster: "ap2",
+    useTLS: true
+  });
+
+
+  const db = mongoose.connection;
+  db.once("open", () => {
+    console.log("db connected");
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on('change', (change) => {
+        console.log("change:",change);
+        if(change.operationType === "insert"){
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {     
+                name: messageDetails.name,
+                message: messageDetails.message
+            });
+        }else{
+            console.log("Error triggering pusher");
+        }
+    })
+  })
+
+
+//Used to handle CORS error. We will allow request to come from any end point.
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+})
 
 const port = 3000 ||process.env.PORT; 
 
